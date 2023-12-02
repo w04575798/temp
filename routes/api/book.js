48bc5ccdbdb0e24007f6a265887aeb885798e1a7
Omar <w0475798@campus.nscc.ca>
2023-12-02 18:37:13 -0400

    assignment2 req3getondocbyId

--- routes/api/book.js
+++ routes/api/book.js
@@ -13,6 +13,23 @@ router.get('/', async (req, res) => {
       res.status(500).json({ error: err.message });
     }
   });
+
+// Get one document by Id
+router.get('/:id', async (req, res) => {
+  console.log('GET /api/book/:id route reached');
+  try {
+    const book = await Book.findOne({ id: req.params.id }).exec();
+    if (!book) {
+      return res.status(404).json({ error: 'Book not found' });
+    }
+    res.send(book); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
 // Create new document
   router.post('/', async (req, res) => {
     try {
