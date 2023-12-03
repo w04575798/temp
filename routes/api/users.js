5c0ed104177e3989729d0931f2fed0b4f40fa6be
Omar <w0475798@campus.nscc.ca>
2023-12-03 11:54:20 -0400

    assignment2phase2 login and final

--- routes/api/users.js
+++ routes/api/users.js
@@ -35,7 +35,27 @@ router.post('/register', async (req, res) => {
 
 // Login endpoint
 router.post('/login', async (req, res) => {
-  
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email: req.body.email });
+
+    // If the user is not found or password is incorrect, send an error response
+    if (!user || !(await bcrypt.compare(req.body.password, user.password))) {
+      return res.status(401).json({ error: 'Invalid credentials' });
+    }
+
+    // Generate a JWT token using the secret key from the environment variable
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    // Set the token in the response header
+    res.header('x-auth-token', token);
+
+    // Send a success response
+    res.status(200).json({ message: 'Login successful' });
+  } catch (error) {
+    // Handle errors
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
